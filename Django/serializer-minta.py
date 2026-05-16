90183b28b01a955f6ca8c6491fa84af1ac3a3e6f
koszkrisz <koszkrisz@gmail.com>
2026-05-16 20:25:26 +0200

    dj rest framework serializer

---
+++ Django/serializer-minta.py
@@ -0,0 +1,10 @@
+# Django REST Framework serializer minta
+
+from rest_framework import serializers
+from .models import Project
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = "__all__"
