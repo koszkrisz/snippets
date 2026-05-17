0e203d5b6eb5a7e96e25b65afb86022e664828b9
koszkrisz <koszkrisz@gmail.com>
2026-05-17 08:32:34 +0200

    data grid  xaml-el

--- WPF/datagrid-itemsource.cs
+++ WPF/datagrid-itemsource.cs
@@ -19,3 +19,39 @@ versenyzok.Add(new Versenyzo()
 // DataGrid neve: dgVersenyzok
 
 dgVersenyzok.ItemsSource = versenyzok;
+
+//xaml-ben a DataGrid oszlopainak definiálása, ha automatikus oszlopgenerálás van:
+
+<DataGrid x:Name="dgHajok"
+          Grid.Column="0"
+          SelectionMode="Single"
+          SelectionUnit="FullRow"
+          AutoGenerateColumns="True"
+          CanUserAddRows="False"
+          SelectionChanged="DataGrid_SelectionChanged"/>
+
+// Ha manuális oszlopdefiníciót szeretnénk:
+<DataGrid x:Name="dgHajok"
+          Grid.Column="0"
+          SelectionMode="Single"
+          SelectionUnit="FullRow"
+          AutoGenerateColumns="False"
+          CanUserAddRows="False"
+          CanUserDeleteRows="False"
+          SelectionChanged="DataGrid_SelectionChanged">
+
+    <DataGrid.Columns>
+
+        <DataGridTextColumn Header="Név"
+                            Binding="{Binding Nev}" />
+                            Width="*"/>         
+        <DataGridTextColumn Header="Típus"
+                            Binding="{Binding Tipus}" />
+                            Width="*"/>
+        <DataGridTextColumn Header="Év"
+                            Binding="{Binding Ev}" />
+                            Width="*"/>
+
+    </DataGrid.Columns>
+
+</DataGrid>
