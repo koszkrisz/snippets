bddf180724ec7500ce1c8ad48a82a6526dad20bb
koszkrisz <koszkrisz@gmail.com>
2026-05-17 06:49:10 +0200

    Ez jobb

--- C#/adatosztaly minta.cs
+++ C#/adatosztaly minta.cs
@@ -1,98 +1,28 @@
-class Konyv
-{
-    public string Cim { get; set; }
-    public string Nev { get; set; }
-    public string Nemzetiseg { get; set; }
-    public int SzulEv { get; set; }
-    public int HalEv { get; set; }
-    public int Helyezes { get; set; }
-
-internal class Feladat
-{
-    private List<Konyv> adatok = [];
-
-    public Feladat()
+    internal class HajoAdat
     {
-        foreach (var item in File.ReadAllLines("kiadas.txt"))
+        public HajoAdat(string hajoNev, string tipus, int gyartasEv, bool uzemel, string tulajdonos, string varos)
         {
-            string[] resz = item.Split(";");
-            int ev = Convert.ToInt32(resz[0]);
-            int negyedEv = Convert.ToInt32(resz[1]);
-            bool kulfoldi = resz[2] == "kf";
-            string leiras = resz[3];
-            int peldany = Convert.ToInt32(resz[4]);
-            adatok.Add(new(ev,negyedEv,kulfoldi,leiras,peldany));
+            HajoNev = hajoNev;
+            Tipus = tipus;
+            GyartasEv = gyartasEv;
+            Uzemel = uzemel;
+            Tulajdonos = tulajdonos;
+            Varos = varos;
         }
-    }
 
-    public void Feladat2()
-    {
-        Console.WriteLine("2. feladat:");
-        Console.Write("Szerző: ");
-        string szerzo = Console.ReadLine();
-        int kiadasok = adatok.Count(x => x.Leiras.Contains(szerzo));
-        if (kiadasok == 0)
-            Console.WriteLine("Nem adtak ki.");
-        else
-            Console.WriteLine($"{kiadasok} könyvkiadás.");
-    }
-
-    public void Feladat3()
-    {
-        int maxKiadas = adatok.Max(x => x.Peldany);
-        int maxKiadasokSzama = adatok.Count(x => x.Peldany == maxKiadas);
-        Console.WriteLine("3. feladat:");
-        Console.WriteLine($"Legnagyobb példányszám: {maxKiadas}, előfordult {maxKiadasokSzama} alkalommal");
-
-    }
-
-    public void Feladat4()
-    {
-        var elso = adatok.Where(x => x.Kulfoldi && x.Peldany >= 40000).First();
-        Console.WriteLine("4. feladat:");
-        Console.WriteLine($"{elso.Ev}/{elso.NegyedEv}. {elso.Leiras}");
-    }
-
-    public void Feladat5()
-    {
-        string html = "";
-        Console.WriteLine("Év\tMagyar kiadás\tMagyar példány\tKülföldi kiadás\tKülföldi példányszám");
-        html += """
-                  <table>
-                    <tr>
-                        <th>Év</th>
-                        <th>Magyar kiadások</th>
-                        <th>Magyar példány</th>
-                        <th>Külföldi kiadás</th>
-                        <th>Külföldi példányszám</th>
-                    </tr>
-                  """;
-        foreach (var item in adatok.GroupBy(x=>x.Ev))
+        public bool Kiemelt()
         {
-            int magyarKiadas = item.Where(x => !x.Kulfoldi).Count();
-            int magyarPeldany = item.Where(x => !x.Kulfoldi).Sum(x=>x.Peldany);
-            int kulfoldiKiadas = item.Where(x => x.Kulfoldi).Count();
-            int kulfoldiPeldany = item.Where(x => x.Kulfoldi).Sum(x => x.Peldany);
-            Console.WriteLine($"{item.Key}\t\t{magyarKiadas}\t\t{magyarPeldany}\t\t{kulfoldiKiadas}\t\t{kulfoldiPeldany}");
-            html += $"""
-                  <tr>
-                    <td>{item.Key}</td>
-                    <td>{magyarKiadas}</td>
-                    <td>{magyarPeldany}</td>
-                    <td>{kulfoldiKiadas}</td>
-                    <td>{kulfoldiPeldany}</td>
-                  </tr>
-                  """;
+            return GyartasEv < 1990 && Uzemel;
         }
-        html += "</table>";
-        StreamWriter sw = new("tabla.html");
-        sw.WriteLine(html);
-        sw.Close();
+
+        public string HajoNev {  get; set; }
+        public string Tipus {  get; set; }
+        public int GyartasEv {  get; set; }
+        public bool Uzemel {  get; set; }
+        public string Tulajdonos {  get; set; }
+        public string Varos {  get; set; }
     }
-
-
-    namespace konyvek
-{
+}
     internal class Program
     {
         static void Main(string[] args)
@@ -105,3 +35,110 @@ internal class Feladat
         }
     }
 }
+namespace Hajo
+{
+    internal class Feladat
+    {
+        List<HajoAdat> adatok = [];
+
+        public Feladat()
+        {
+            foreach (var item in File.ReadAllLines("hajolista.csv").Skip(1))
+            {
+                string[] resz = item.Split(';');
+                string hajonev = resz[0];
+                string tipus = resz[1];
+                int gyartasEv = Convert.ToInt32(resz[2]);
+                bool uzemel = resz[3] == "1";
+                string tulajdonos = resz[4];
+                string varos = resz[5];
+                HajoAdat uj = new(hajonev, tipus, gyartasEv, uzemel, tulajdonos, varos);
+                adatok.Add(uj);
+            }
+        }
+
+        public void Feladat4()
+        {
+            Console.WriteLine($"4. feladat: Hajók száma: {adatok.Count}");
+        }
+
+        public void Feladat5()
+        {
+            var eredmeny = adatok.Where(x => x.Varos != "Siófok");
+            Console.WriteLine("5. feladat: Siófokon kívüli hajók:");
+            foreach (var item in eredmeny)
+            {
+                Console.WriteLine(item.HajoNev);
+            }
+        }
+
+        public void Feladat7()
+        {
+            Console.Write("7. feladat: Kérem a város nevét: ");
+            string varos = Console.ReadLine();
+            if (adatok.Any(x=>x.Varos==varos)) // if (adatok.Count(x=>x.Varos)!=0)
+            {
+                double atlagosEv = adatok.
+                    Where(x => x.Varos == varos).
+                    Average(x => x.GyartasEv);
+                Console.WriteLine($"A hajók átlagos gyártási éve: {atlagosEv:N0}");
+            }
+            else
+            {
+                Console.WriteLine("Nem található hajó a megadott városban");
+            }
+        }
+
+        //8. feladat: Jelenjenek meg Balatonfüred város hajói a gyártási év szerinti csökkenő sorrendben, azon belül pedig névsorrendben! Jelenjen meg a hajó neve és a gyártási év!
+
+        public void Feladat8()
+        {
+            var eredmeny = adatok.
+                Where(x => x.Varos == "Balatonfüred").
+                OrderByDescending(x => x.GyartasEv).
+                ThenBy(x => x.HajoNev);
+            Console.WriteLine("8. feladat: Balatonfüred hajói:");
+            foreach (var item in eredmeny)
+            {
+                Console.WriteLine($"{item.HajoNev} - {item.GyartasEv}");
+            }
+        }
+
+        //9. feladat: Jelenjen meg, hogy melyik az az év, amelyben az utolsó hajót vagy hajókat gyártották, és utána jelenjenek meg azoknak a hajóknak a neve névsorrendben, amelyeket ebben az évben gyártottak!
+
+        public void Feladat9()
+        {
+            int maxEv = adatok.Max(x => x.GyartasEv);
+            Console.WriteLine($"9. feladat: Utolsó gyártási év: {maxEv}");
+            var eredmeny = adatok.Where(x => x.GyartasEv == maxEv).OrderBy(x => x.HajoNev);
+            foreach (var item in eredmeny)
+            {
+                Console.WriteLine(item.HajoNev);
+            }
+        }
+
+        //Jelenjen meg az összes hajótípus és az, hogy mennyi hajó tartozik ehhez a típushoz!
+
+        public void Feladat10()
+        {
+            Console.WriteLine("10. feladat: Hajótípusok:");
+            foreach (var item in adatok.GroupBy(x=>x.Tipus))
+            {
+                Console.WriteLine($"{item.Key} - {item.Count()} darab");
+            }
+        }
+
+        //Készíts állományt kiemelt.csv néven, amelybe kerüljenek bele a kiemelt támogatású hajók neve, gyártási éve, tulajdonosa és városa ;-vel elválasztva! Az első sorban legyenek megjelenítve a mezőnevek!
+
+        public void Feladat11()
+        {
+            StreamWriter sw = new("kiemelt.csv");
+            sw.WriteLine("hajó neve;gyártás_éve;tulajdonos;város");
+            var eredmeny = adatok.Where(x => x.Kiemelt());
+            foreach (var item in eredmeny)
+            {
+                sw.WriteLine(string.Join(';',item.HajoNev,item.GyartasEv,item.Tulajdonos,item.Varos));
+            }
+            sw.Close();
+        }
+
